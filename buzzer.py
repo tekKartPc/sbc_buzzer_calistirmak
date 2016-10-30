a78023a0158ef103f9dc8255c289057bb18db2e0
tekkartpc <tekkartpc@openaliasbox.org>
2016-10-30 09:57:51 +0300

    yeni duzenleme

---
+++ buzzer.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*- 
+# tekKartPc.com
+# twitter.com/tekKartPc
+# facebokk.com/tekKartPc
+# -------------
+# buzzer çalıştırma için önce
+# gerekli bağlantıyı yapınız.
+# detaylı bilgi için http://tekkartpc.com/raspberry-pi-ile-led-yakmak-ve-buzzer-calistirmak/ adresini ziyaret edip sorularınızı sorabilirsiniz.
+
+import RPi.GPIO as GPIO
+import time
+
+hangiPin = 18
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(hangiPin,GPIO.OUT)
+
+try:
+
+	while True:
+
+		GPIO.output(hangiPin,True)
+ 		time.sleep(1)
+ 		GPIO.output(hangiPin,False)
+ 		time.sleep(1)
+
+except KeyboardInterrupt: 
+#cikis yapildi
+	GPIO.output(hangiPin,False)
+	print("buzzer uygulamasindan cikis yapildi")
+finally:
+	GPIO.cleanup()
